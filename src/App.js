f66a1becd12c40827f17d44b78798a4cdd3c2297
Ryan Perry <rperry2174@gmail.com>
2022-04-20 12:32:14 -0700

    Add diff example

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React from "react";
 import { FlamegraphRenderer } from "@pyroscope/flamegraph";
 import "@pyroscope/flamegraph/dist/index.css";
 import { pyroscopeCPU } from "./TestData";
+import { pyroscopeDiffCPU } from "./TestDiffData";
 
 function App() {
   return (
     <div>
+      <h1>Pyroscope Flamegraph</h1>
       <FlamegraphRenderer
         profile={pyroscopeCPU}
+        viewType="single"
+        display="flamegraph"
+        showToolbar={false}
+      />
+      <h1>Pyroscope Diff Flamegraph</h1>
+      <FlamegraphRenderer
+        profile={pyroscopeDiffCPU}
         viewType="single"
         display="flamegraph"
         showToolbar={false}
